205836899c8d8fc179d351aee67c0ebf92697156
sholikun32 <102804771+sholikun32@users.noreply.github.com>
2023-12-16 20:32:57 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,48 +1,48 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.impute import SimpleImputer
 
-# Load the data
-file_path = 'KUESIONER_TB_TOTAL(1) 27 oktober.xlsx'
-df = pd.read_excel(file_path)
+# Function to load data
+@st.cache
+def load_data(uploaded_file):
+    if uploaded_file is not None:
+        df = pd.read_excel(uploaded_file)
+        return df
+    return None
 
-# Streamlit code for TB data processing
-st.title('TB Data Processing Intelligence System')
+# Streamlit code for TB data processing and visualization
+st.title('TB Data Processing and Visualization')
 
-# Display data tables
-st.header('Data Overview')
-st.write(df.head())
-
-# Data Preprocessing
-st.header('Data Preprocessing')
-# Handling missing values
-imputer = SimpleImputer(strategy='most_frequent')
-df_imputed = pd.DataFrame(imputer.fit_transform(df), columns=df.columns)
-st.write('Missing values handled.')
-
-# Encoding categorical variables
-label_encoders = {}
-for column in df.select_dtypes(include=['object']).columns:
-    label_encoders[column] = LabelEncoder()
-    df_imputed[column] = label_encoders[column].fit_transform(df_imputed[column])
-st.write('Categorical variables encoded.')
-
-# Data Visualization
-st.header('Data Visualization')
-# Distribution of age
-st.subheader('Age Distribution')
-fig, ax = plt.subplots()
-sns.histplot(df_imputed['Usia'], kde=True, ax=ax)
-st.pyplot(fig)
-
-# Save the Streamlit script to a file
-streamlit_script = 'tb_questionnaire_streamlit.py'
-with open(streamlit_script, 'w') as file:
-    file.write(streamlit_code)
-
-print('Streamlit script saved as:', streamlit_script)
+# Upload file
+uploaded_file = st.file_uploader('Choose a file')
+if uploaded_file is not None:
+    data = load_data(uploaded_file)
+    if data is not None:
+        st.write('Raw Data')
+        st.write(data)
+        
+        st.header('Data Overview')
+        st.write(data.head())
+        
+        st.header('Data Preprocessing')
+        imputer = SimpleImputer(strategy='most_frequent')
+        df_imputed = pd.DataFrame(imputer.fit_transform(data), columns=data.columns)
+        
+        label_encoders = {}
+        for column in df_imputed.select_dtypes(include=['object']).columns:
+            label_encoders[column] = LabelEncoder()
+            df_imputed[column] = label_encoders[column].fit_transform(df_imputed[column])
+        
+        st.write('Preprocessed Data')
+        st.write(df_imputed.head())
+        
+        st.header('Data Visualization')
+        column_to_plot = st.selectbox('Select a column to visualize', df_imputed.columns)
+        fig, ax = plt.subplots()
+        sns.histplot(df_imputed[column_to_plot], kde=True, ax=ax)
+        ax.set_title('Histogram of ' + column_to_plot)
+        st.pyplot(fig)
